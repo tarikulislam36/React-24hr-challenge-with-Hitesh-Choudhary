9fe4a759c40bd1d8e9c3262017c98a5495854a95
tarikulislam <tarikulislam4145@gmail.com>
2024-02-26 00:24:25 +0530

    create component

--- 01_test/test_01/src/App.jsx
+++ 01_test/test_01/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import CreateComponent from './Ceate_component'
+
 
 
 function App() {
  
   return (
     <>
-     <h1>Hello Tarikul</h1>
+    <CreateComponent />
     </>
   )
 }
---
+++ 01_test/test_01/src/Ceate_component.jsx
@@ -0,0 +1,5 @@
+function CreateComponent() {
+  return <div><h1>  Component creation  </h1></div>;
+}
+
+export default CreateComponent;
